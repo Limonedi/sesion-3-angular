ba64ed11f182c32666adba6e1021a876965a468b
Limonedi <holuloldoce@gmail.com>
2022-12-23 18:57:00 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { noop } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'c3a';
+  //cursos!:string[];
+  cursos = ['Javascript', 'C#']
+  addCurso(nombrec:string){
+    //alert(nombrec);
+    this.cursos.push(String(nombrec));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BodyModule } from './body/body.module';
+import { FooterModule } from './footer/footer.module';
+import { HeaderModule } from './header/header.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        HeaderModule,
+        BodyModule,
+        FooterModule
+    ]
 })
 export class AppModule { }
